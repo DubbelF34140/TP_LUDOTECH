db26ead55c501d02e6048ff0b554226270996431
DubbelF34140 <yw34140@gmail.com>
2025-03-19 16:02:03 +0100

    init

---
+++ src/main/java/dubbelf/fr/ludotechtp/LudoTechTpApplication.java
@@ -0,0 +1,13 @@
+package dubbelf.fr.ludotechtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudoTechTpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LudoTechTpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dubbelf/fr/ludotechtp/LudoTechTpApplicationTests.java
@@ -0,0 +1,13 @@
+package dubbelf.fr.ludotechtp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LudoTechTpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
